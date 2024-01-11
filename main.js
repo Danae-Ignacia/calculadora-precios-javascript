81b8f3e7c3a9c8a58c63bb67bf5ec548cacc5e59
Danae Quevedo <d.ignacia.quevedo.v@gmail.com>
2024-01-10 23:19:48 -0300

    inicio proyecto

---
+++ main.js
@@ -0,0 +1,12 @@
+let cliente = prompt("Ingresa tu nombre").toLowerCase();
+if (cliente != ""){
+        alert("Gracias por utilizar la calculadora de precios");
+}else{
+    alert("Debes ingresar tu nombre");
+}
+
+// let precio = prompt("ingresa el precio de tu plan");
+// let descuento = prompt("ingresa el descuento");
+// function descuento(precio, descuento){
+// return (precio - descuento)
+// }
