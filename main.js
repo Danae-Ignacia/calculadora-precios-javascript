fc2b353491975fe0dbc41c781494c0c97a299cd0
Danae Quevedo <d.ignacia.quevedo.v@gmail.com>
2024-01-11 21:14:03 -0300

    mejoras en sección descuentos

--- main.js
+++ main.js
@@ -1,12 +1,24 @@
 let cliente = prompt("Ingresa tu nombre").toLowerCase();
-if (cliente != ""){
-        alert("Gracias por utilizar la calculadora de precios");
-}else{
+if (cliente != "") {
+    alert("Gracias por utilizar la calculadora de precios");
+} else {
     alert("Debes ingresar tu nombre");
 }
 
-// let precio = prompt("ingresa el precio de tu plan");
-// let descuento = prompt("ingresa el descuento");
-// function descuento(precio, descuento){
-// return (precio - descuento)
-// }
+let plan = prompt("ingresa el nombre de tu plan").toLowerCase();
+
+while (plan != "ESC") {
+    switch (plan) {
+        case "basico":
+            alert("Tu descuento será del 20%");
+            break;
+        case "estandar":
+            alert("Tu descuento será del 30%");
+            break;
+        default:
+            alert("Tu descuento será del 50%");
+            break;
+    }
+
+    plan = prompt("ingresa el nombre de tu plan").toLowerCase;
+}
